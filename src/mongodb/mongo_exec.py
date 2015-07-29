5e555f2628e9be91155ae41ffbe2f289da5ddb72
Jan_Hybs <jan.hybs@tul.cz>
2015-07-29 11:55:09 +0200

    Remake module into class, add duck-type methods

--- src/mongodb/mongo_exec.py
+++ src/mongodb/mongo_exec.py
@@ -3,28 +3,45 @@
 
 import pymongo
 from pymongo import MongoClient
-client = MongoClient('127.0.0.1', 27017)
-db = client.test
-collection = db.test
 
-def add_measurements (json_data):
-    data = json_data.copy()
-    data.pop('children', None)
-    # data['_id'] = data['task-description']
 
-    inserted_id = collection.insert_one (data).inserted_id
+class MongoExec(object):
+    def __init__(self):
+        self.client = MongoClient('127.0.0.1', 27017)
+        self.db = self.client.test
+        self.collection = self.db.test
 
-    if 'children' in json_data:
-        for child in json_data['children']:
-            add_measurement(child, inserted_id)
+    def process_file(self, json_data):
+        self.add_measurements(json_data)
 
-def add_measurement (json_data, parent_id):
-    data = json_data.copy()
-    data.pop('children', None)
-    data['parent'] = parent_id
-    # data['_id'] = data.pop('tag')
-    inserted_id = collection.insert_one (data).inserted_id
+    def close(self):
+        pass
 
-    if 'children' in json_data:
-        for child in json_data['children']:
-            add_measurement(child, inserted_id)
+    def commit(self):
+        pass
+
+
+    # ------------------------------
+
+
+    def add_measurements(self, json_data):
+        data = json_data.copy()
+        data.pop('children', None)
+        # data['_id'] = data['task-description']
+
+        inserted_id = self.collection.insert_one(data).inserted_id
+
+        if 'children' in json_data:
+            for child in json_data['children']:
+                self.add_measurement(child, inserted_id)
+
+    def add_measurement(self, json_data, parent_id):
+        data = json_data.copy()
+        data.pop('children', None)
+        data['parent'] = parent_id
+        # data['_id'] = data.pop('tag')
+        inserted_id = self.collection.insert_one(data).inserted_id
+
+        if 'children' in json_data:
+            for child in json_data['children']:
+                self.add_measurement(child, inserted_id)
--- src/mysqldb/mysql_exec.py
+++ src/mysqldb/mysql_exec.py
@@ -6,64 +6,73 @@ from config import credentials
 from mysqldb.mysql_query import insert_condition_fields, insert_condition_query, insert_measurement_query, \
     insert_structure_query
 
-connector = mysql.connector.connect(**credentials)
-cursor = connector.cursor()
+
+class MySQLExec(object):
+    def __init__(self):
+        self.connector = mysql.connector.connect(**credentials)
+        self.cursor = self.connector.cursor()
 
 
+    def process_file(self, json_data):
+        whole_program = json_data['children'][0]
+        condition_id = self.create_conditions(json_data)
+        self.create_structure(whole_program, parent=None)
+        self.add_measurements(whole_program, condition_id)
 
-def create_conditions(json_data):
-    data = { key: json_data[key] for key in insert_condition_fields }
-    cursor.execute(insert_condition_query, data)
+    def close(self):
+        self.cursor.close()
+        self.connector.close()
 
-    return cursor.lastrowid
+    def commit(self):
+        self.connector.commit()
 
-
-def create_structure(json_data, parent=None):
-    try:
-        cursor.execute(insert_structure_query, { 'name': json_data['tag'], 'parent': parent })
-    except mysql.connector.errors.IntegrityError as e:
-        # print e
-        pass
-
-    if 'children' in json_data:
-        for child in json_data['children']:
-            create_structure(child, json_data['tag'])
+    # ------------------------------
 
 
-def create_measurement(json_data, metric, condition_id):
-    return {
-        'metric': metric,
-        'value': json_data[metric],
-        'structure': json_data['tag'],
-        'cond': condition_id
-    }
+    def create_conditions(self, json_data):
+        data = { key: json_data[key] for key in insert_condition_fields }
+        self.cursor.execute(insert_condition_query, data)
+
+        return self.cursor.lastrowid
 
 
-def add_measurements(json_data, condition_id):
-    # store all keys
-    fields = set(json_data.keys())
-    fields = fields - set(['function', 'tag', 'file-path'])
-
-    # except children
-    if 'children' in fields:
-        fields.remove ('children')
-
-    for metric in fields:
+    def create_structure(self, json_data, parent=None):
         try:
-            data = create_measurement(json_data, metric, condition_id)
-            cursor.execute(insert_measurement_query, data)
-        except mysql.connector.errors.DatabaseError as e:
-            print "{:s} {:s}".format(data, e)
+            self.cursor.execute(insert_structure_query, { 'name': json_data['tag'], 'parent': parent })
+        except mysql.connector.errors.IntegrityError as e:
+            # print e
+            pass
+
+        if 'children' in json_data:
+            for child in json_data['children']:
+                self.create_structure(child, json_data['tag'])
 
 
-    if 'children' in json_data:
-        for child in json_data['children']:
-            add_measurements(child, condition_id)
+    def create_measurement(self, json_data, metric, condition_id):
+        return {
+            'metric': metric,
+            'value': json_data[metric],
+            'structure': json_data['tag'],
+            'cond': condition_id
+        }
 
 
-def mysql_close ():
-    cursor.close()
-    connector.close()
+    def add_measurements(self, json_data, condition_id):
+        # store all keys
+        fields = set(json_data.keys())
+        fields = fields - set(['function', 'tag', 'file-path'])
 
-def mysql_commit ():
-    connector.commit()
+        # except children
+        if 'children' in fields:
+            fields.remove('children')
+
+        for metric in fields:
+            try:
+                data = self.create_measurement(json_data, metric, condition_id)
+                self.cursor.execute(insert_measurement_query, data)
+            except mysql.connector.errors.DatabaseError as e:
+                print "{:s} {:s}".format(data, e)
+
+        if 'children' in json_data:
+            for child in json_data['children']:
+                self.add_measurements(child, condition_id)
